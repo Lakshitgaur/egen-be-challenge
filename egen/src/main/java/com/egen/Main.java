29f6a597af84c0567544496b65af9acbb91256fd
Lakshitgaur <lgaur88@ccs.neu.edu>
2016-07-31 22:03:00 -0400

    user class creation
    User class creation

---
+++ egen/src/main/java/com/egen/Main.java
@@ -0,0 +1,8 @@
+package com.egen;
+
+public class Main {
+	public static void main(String[] args){
+		new UserServiceProvider(new UserServices());
+			
+		}
+	}
---
+++ egen/src/main/java/com/egen/User.java
@@ -0,0 +1,5 @@
+package com.egen;
+
+public class User {
+
+}
